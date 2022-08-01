4f4cf93d3154e6b7e5b0f95795a101eb787f0c22
eslamyounis1 <eslamupwork@gmail.com>
2022-08-01 23:58:32 +0200

    recent_order

--- lib/widgets/recent_orders.dart
+++ lib/widgets/recent_orders.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/data/data.dart';
+import 'package:flutter_food_delivery_ui/models/order.dart';
 
 class RecentOrders extends StatelessWidget {
   const RecentOrders({Key? key}) : super(key: key);
@@ -7,8 +10,8 @@ class RecentOrders extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: const [
-        Padding(
+      children: [
+        const Padding(
           padding: EdgeInsets.symmetric(
             horizontal: 20.0,
           ),
@@ -20,8 +23,46 @@ class RecentOrders extends StatelessWidget {
             ),
           ),
         ),
-        
+        Container(
+          height: 120,
+          // color: Colors.blue,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: currentUser.orders.length,
+            itemBuilder: (context, index) {
+              Order order = currentUser.orders[index];
+              return _buildRecentOrder(context, order);
+            },
+          ),
+        ),
       ],
     );
   }
+
+  _buildRecentOrder(context, Order order) => Container(
+        margin: const EdgeInsets.all(8.0),
+        width: 320.0,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15.0),
+          border: Border.all(
+            width: 1.0,
+            color: Colors.grey[200]!,
+            style: BorderStyle.solid,
+          ),
+        ),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image(
+                height: 100.0,
+                width: 100.0,
+                image: AssetImage(order.food.imageUrl),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
+      );
 }
