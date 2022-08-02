b9dfdeaba856080f181b28b4ae959ed3073c0533
eslamyounis1 <eslamupwork@gmail.com>
2022-08-02 16:56:33 +0200

    edit_feature_commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery_ui/data/data.dart';
 import 'package:flutter_food_delivery_ui/models/restaurant.dart';
+import 'package:flutter_food_delivery_ui/screens/resturant_screen.dart';
 
 import '../widgets/rating_stars.dart';
 import '../widgets/recent_orders.dart';
@@ -105,82 +106,94 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget _buildRestaurants() {
     List<Widget> restaurantList = [];
-    restaurants.forEach((Restaurant restaurant) {
+    for (var restaurant in restaurants) {
       restaurantList.add(
-        Container(
-          margin: const EdgeInsets.symmetric(
-            horizontal: 20.0,
-            vertical: 10.0,
-          ),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(15.0),
-            border: Border.all(
-              width: 1.0,
-              color: Colors.grey[200]!,
-            ),
-          ),
-          child: Row(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(15.0),
-                child: Image(
-                  height: 150.0,
-                  width: 150.0,
-                  image: AssetImage(
-                    restaurant.imageUrl,
-                  ),
-                  fit: BoxFit.cover,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => RestaurantScreen(
+                  restaurant: restaurant,
                 ),
               ),
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.all(12.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        restaurant.name,
-                        style: const TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
+            );
+          },
+          child: Container(
+            margin: const EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 10.0,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15.0),
+              border: Border.all(
+                width: 1.0,
+                color: Colors.grey[200]!,
+              ),
+            ),
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Image(
+                    height: 150.0,
+                    width: 150.0,
+                    image: AssetImage(
+                      restaurant.imageUrl,
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          restaurant.name,
+                          style: const TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 4.0,
-                      ),
-                      RatingStars(rating: restaurant.rating),
-                      const SizedBox(
-                        height: 4.0,
-                      ),
-                      Text(
-                        restaurant.address,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
+                        const SizedBox(
+                          height: 4.0,
+                        ),
+                        RatingStars(rating: restaurant.rating),
+                        const SizedBox(
+                          height: 4.0,
+                        ),
+                        Text(
+                          restaurant.address,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
                             fontSize: 16.0,
                             fontWeight: FontWeight.w600,
-                      ),
-                      ),
-                      const SizedBox(
-                        height: 4.0,
-                      ),
-                      const Text(
-                        '0.2 miles away',
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w500,
+                          ),
                         ),
-                      ),
-                    ],
+                        const SizedBox(
+                          height: 4.0,
+                        ),
+                        const Text(
+                          '0.2 miles away',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       );
-    });
+    }
     return Column(
       children: restaurantList,
     );
